13e606388fac2dfe4900515b32f82ad22309846a
Evgeniy Kartashov <Evgeniy.Kartashov@netwrix.com>
2019-02-13 21:16:02 +0300

    refactoring User

--- CollectorBot/CollectorBot/Model/DataBase/User.cs
+++ CollectorBot/CollectorBot/Model/DataBase/User.cs
@@ -2,6 +2,6 @@ namespace CollectorBot.Model.DataBase {
     public class User {
         public string Id { get; set; }
         public long TelegramUserId { get; set; }
-        public string UserName { get; set; }
+        public string Name { get; set; }
     }
 }
