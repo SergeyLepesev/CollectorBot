fe63180a74e7548882778a7a9e12a3b52579734d
Sergey Lepesev <lepesevss@yandex.ru>
2019-02-13 13:43:30 +0300

    Init asp.net core v2.2 solution

---
+++ CollectorBot/CollectorBot/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectorBot.Controllers {
+    [Route("api/health")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase {
+        [HttpGet("check")]
+        public IActionResult HealthCheck() {
+            return Ok();
+        }
+    }
+}
